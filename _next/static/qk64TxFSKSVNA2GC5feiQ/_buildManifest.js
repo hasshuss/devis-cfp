d978d67829c64b72bce12ec062711aef65ecb3cc
hasshuss <46470134+hasshuss@users.noreply.github.com>
2026-06-06 10:31:15 +0200

    Add files via upload

---
+++ _next/static/qk64TxFSKSVNA2GC5feiQ/_buildManifest.js
@@ -0,0 +1,35 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/e2929d2e572f4235.js"
+  ],
+  "/_error": [
+    "static/chunks/d25e95cfdbacf35e.js"
+  ],
+  "/cfp": [
+    "static/chunks/2a247c6f9097e0d9.js"
+  ],
+  "/devis-cfp": [
+    "static/chunks/355585248766405c.js"
+  ],
+  "/mouns": [
+    "static/chunks/4d60af3fca0c6686.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/devis-cfp//_next/:path+",
+        "destination": "/devis-cfp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/cfp",
+    "/devis-cfp",
+    "/mouns"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
