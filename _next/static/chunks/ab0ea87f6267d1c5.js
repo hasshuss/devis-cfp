9958f6f67733c190dbe0dbe8fea7c2ee96acd237
hasshuss <46470134+hasshuss@users.noreply.github.com>
2026-06-06 10:00:45 +0200

    Add files via upload

---
+++ _next/static/chunks/ab0ea87f6267d1c5.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/devis-cfp", [
+  "static/chunks/c206b72d34c7f6fb.js",
+  "static/chunks/62076349db94f65a.js",
+  "static/chunks/30da949fa538349f.js",
+  "static/chunks/a85b56c244bab9fa.js",
+  "static/chunks/ad5ede091c2a4ca4.js",
+  "static/chunks/turbopack-639af40d015e1c54.js"
+])
